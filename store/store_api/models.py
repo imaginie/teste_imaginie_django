ada3f2e31e18102bbc7afe1309a5b3933e5cfa96
Guilherme Kingma <guilherme.kingma@imaginie.com>
2018-01-23 11:32:50 -0200

    Adiciona código

---
+++ store/store_api/models.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Genre(models.Model):
+    pass
+
+
+class Band(models.Model):
+    pass
+
+
+class Music(models.Model):
+    pass
+
+
+class Playlist(models.Model):
+    pass
